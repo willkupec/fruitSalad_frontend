1c1367b7ff2045a7c82a78840b04e9cdad0c0858
willkupec <willem.kupec@gmail.com>
2023-08-22 12:05:50 +0200

    [FS-25] created keycloak private route

--- src/App.js
+++ src/App.js
@@ -3,26 +3,38 @@ import { Route, Routes, useLocation } from "react-router-dom"
 import Login from "./pages/Login/Login"
 import Registration from "./pages/Registration/Registration"
 import Home from "./pages/Home/Home"
+import { ReactKeycloakProvider } from "@react-keycloak/web"
+import keycloak from "./Keycloak"
 import { LOGIN, REGISTRATION } from "./constants/frontend_routes"
 import Header from "./components/Header/Header"
 import UserProvider from "./context/UserContext/UserProvider"
 import CartProvider from "./context/CartContext/CartProvider"
 import Cart from "./components/Cart/Cart"
+import PrivateRoute from "./utils/PrivateRoute"
 
 const App = () => {
   const location = useLocation()
   return (
-    <UserProvider>
-      <CartProvider>
-        <Cart />
-        <Header />
-        <Routes location={location}>
-          <Route path={"*"} element={<Home />} />
-          <Route path={LOGIN} element={<Login />} />
-          <Route path={REGISTRATION} element={<Registration />} />
-        </Routes>
-      </CartProvider>
-    </UserProvider>
+    <ReactKeycloakProvider authClient={keycloak}>
+      <UserProvider>
+        <CartProvider>
+          <Cart />
+          <Header />
+          <Routes location={location}>
+            <Route path={"*"} element={<Home />} />
+            <Route path={LOGIN} element={<Login />} />
+            <Route
+              path={REGISTRATION}
+              element={
+                <PrivateRoute>
+                  <Registration />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </CartProvider>
+      </UserProvider>
+    </ReactKeycloakProvider>
   )
 }
 
