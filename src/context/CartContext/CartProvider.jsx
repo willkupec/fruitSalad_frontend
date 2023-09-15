eb7de1b3e23eb7948b8d8a406229bb846e8629b4
Will Kupec <willem.kupec@gmail.com>
2023-09-15 14:34:53 +0200

    [FS-39] Fix keycloak console errors in frontend (#14)
    * [FS-39] fixed keycloak console error
    
    * [FS-39] fixed all keycloak bugs
    
    * [FS-39] added cart to local storage

--- src/context/CartContext/CartProvider.jsx
+++ src/context/CartContext/CartProvider.jsx
@@ -1,10 +1,19 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useState } from "react"
 import CartContext from "./CartContext"
 
+const getInitialCart = () => {
+  const cart = localStorage.getItem("cart")
+  return cart ? JSON.parse(cart) : []
+}
+
 const CartProvider = (props) => {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(getInitialCart())
   const [showCart, setShowCart] = useState(false)
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart))
+  }, [cart])
 
   return (
     <CartContext.Provider value={{ cart, setCart, showCart, setShowCart }}>
